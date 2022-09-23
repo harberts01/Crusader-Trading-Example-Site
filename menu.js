6e5e3f5b3b5159db06cfbb9a329ff2e50ea94d59
Phil Harberts <109708014+harberts01@users.noreply.github.com>
2022-09-23 16:27:51 -0500

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-function menuApp(){
-    let x = document.getElementById('mobileLinks')
-    if (x.style.display === block){
-        x.style.display = 'none';
-    }else{
-        x.style.display = 'block';
-    }
-}
